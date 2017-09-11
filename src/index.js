561b561acfb3500e1c233aeb6a2f81a398bf8067
Andrea <andry.tino@gmail.com>
2017-09-12 00:28:29 +0200

    Advances in charts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,27 @@
  */
 
 window.addEventListener("load", function() {
-    var svg = d3.select("svg"),
+    attachNativeEvents();
+
+    loadDataInChart("#histoLang", [{"letter":"A","frequency":0.08167},{"letter":"B","frequency":0.01492},{"letter":"C","frequency":0.02782},{"letter":"D","frequency":0.04253},{"letter":"E","frequency":0.12702},{"letter":"F","frequency":0.02288},{"letter":"G","frequency":0.02015},{"letter":"H","frequency":0.06094},{"letter":"I","frequency":0.06266},{"letter":"J","frequency":0.00153},{"letter":"K","frequency":0.00772},{"letter":"L","frequency":0.04025},{"letter":"M","frequency":0.02406},{"letter":"N","frequency":0.06749},{"letter":"O","frequency":0.07507},{"letter":"P","frequency":0.01929},{"letter":"Q","frequency":0.00095},{"letter":"R","frequency":0.05987},{"letter":"S","frequency":0.06327},{"letter":"T","frequency":0.09056},{"letter":"U","frequency":0.01258},{"letter":"V","frequency":0.00978},{"letter":"W","frequency":0.0236},{"letter":"X","frequency":0.0015},{"letter":"Y","frequency":0.01974},{"letter":"Z","frequency":0.00074}], "Language");
+});
+
+function attachNativeEvents() {
+    var langCombo = document.getElementById("langCombo");
+    langCombo.addEventListener("change", function(e) {
+        console.log("change on langComgo", e);
+    });
+
+    var textInput = document.getElementById("textInput");
+    textInput.addEventListener("input", function(e) {
+        console.log("input on textInput", e);
+
+        loadDataInChart("#histoText", [{"letter":"A","frequency":0.08167},{"letter":"B","frequency":0.01492},{"letter":"C","frequency":0.02782},{"letter":"D","frequency":0.04253},{"letter":"E","frequency":0.12702},{"letter":"F","frequency":0.02288},{"letter":"G","frequency":0.02015},{"letter":"H","frequency":0.06094},{"letter":"I","frequency":0.06966},{"letter":"J","frequency":0.00153},{"letter":"K","frequency":0.00772},{"letter":"L","frequency":0.04025},{"letter":"M","frequency":0.02406},{"letter":"N","frequency":0.06749},{"letter":"O","frequency":0.07507},{"letter":"P","frequency":0.01929},{"letter":"Q","frequency":0.00095},{"letter":"R","frequency":0.05987},{"letter":"S","frequency":0.06327},{"letter":"T","frequency":0.09056},{"letter":"U","frequency":0.02758},{"letter":"V","frequency":0.00978},{"letter":"W","frequency":0.0236},{"letter":"X","frequency":0.0015},{"letter":"Y","frequency":0.01974},{"letter":"Z","frequency":0.00074}], "Text frequencies");
+    });
+}
+
+function loadDataInChart(svgId, data, title) {
+    var svg = d3.select(svgId),
         margin = {top: 20, right: 20, bottom: 30, left: 40},
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom;
@@ -13,12 +33,6 @@ window.addEventListener("load", function() {
 
     var g = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    d3.tsv("data.tsv", function(d) {
-        d.frequency = +d.frequency;
-        return d;
-    }, function(error, data) {
-    if (error) throw error;
 
     x.domain(data.map(function(d) { return d.letter; }));
     y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
@@ -43,8 +57,20 @@ window.addEventListener("load", function() {
         .enter().append("rect")
         .attr("class", "bar")
         .attr("x", function(d) { return x(d.letter); })
-        .attr("y", function(d) { return y(d.frequency); })
+        .attr("y", height)
         .attr("width", x.bandwidth())
+        .attr("height", 0)
+        .attr("fill", function(d) { return "rgb(0, 0, " + (d.frequency*1000) + ")"; })
+        .transition()
+        .duration(750)
+        .attr("y", function(d) { return y(d.frequency); })
         .attr("height", function(d) { return height - y(d.frequency); });
-    });
-});
+    
+    // Title
+    svg.append("text")
+        .attr("x", (svg.attr("width") / 2))             
+        .attr("y", 12)
+        .attr("text-anchor", "middle")  
+        .style("font-size", ".7em") 
+        .text(title);
+}
