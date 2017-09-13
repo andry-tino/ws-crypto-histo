90a67d0e74231045397638bd9120697431cbce5e
Andrea <andry.tino@gmail.com>
2017-09-13 23:48:20 +0200

    First iteration text stats algorithm

--- src/textStats.js
+++ src/textStats.js
@@ -3,5 +3,19 @@
  */
 
 function calculateTextStats(text) {
-    return [{ "letter": "A", "frequency": 0.08167 }, { "letter": "B", "frequency": 0.01492 }, { "letter": "C", "frequency": 0.02782 }, { "letter": "D", "frequency": 0.04253 }, { "letter": "E", "frequency": 0.12702 }, { "letter": "F", "frequency": 0.02288 }, { "letter": "G", "frequency": 0.02015 }, { "letter": "H", "frequency": 0.06094 }, { "letter": "I", "frequency": 0.06966 }, { "letter": "J", "frequency": 0.00153 }, { "letter": "K", "frequency": 0.00772 }, { "letter": "L", "frequency": 0.04025 }, { "letter": "M", "frequency": 0.02406 }, { "letter": "N", "frequency": 0.06749 }, { "letter": "O", "frequency": 0.07507 }, { "letter": "P", "frequency": 0.01929 }, { "letter": "Q", "frequency": 0.00095 }, { "letter": "R", "frequency": 0.05987 }, { "letter": "S", "frequency": 0.06327 }, { "letter": "T", "frequency": 0.09056 }, { "letter": "U", "frequency": 0.02758 }, { "letter": "V", "frequency": 0.00978 }, { "letter": "W", "frequency": 0.0236 }, { "letter": "X", "frequency": 0.0015 }, { "letter": "Y", "frequency": 0.01974 }, { "letter": "Z", "frequency": 0.00074 }];
+    if (!text) return;
+    console.log("Calculating stats for text length:", text.length);
+
+    // To upper + remove spaces and line breaks
+    var processedText = text.toUpperCase().replace(/\n|\r|\s/g, "");
+    var total = processedText.length;
+    var letters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+
+    var freqs = [];
+    for (let i = 0; i < letters.length; i++) {
+        var frequency = (processedText.match(new RegExp(letters[i], "g")) || []).length/total;
+        freqs.push({ "letter": letters[i], "frequency": frequency });
+    }
+
+    return freqs;
 }
