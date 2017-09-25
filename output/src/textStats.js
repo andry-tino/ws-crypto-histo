3f608e3cf9ebe2d16b5006ac9c6de5fdb43ed8ea
Andrea <andry.tino@gmail.com>
2017-09-25 07:03:13 +0200

    Documenting output textStats more

--- output/src/textStats.js
+++ output/src/textStats.js
@@ -8,6 +8,21 @@ function calculateTextStats(text) {
      * and will pass the text in the argument. 
      * It is your job to code this function and make it return the frequencies for each letter 
      * so that CryptoHisto can display the chart. For now we are returning all 0 frequencies!
+     * 
+     * Toolbox
+     * You can use these functions made for you to make your coding simpler:
+     * - removeWhiteSpaces(text):                       This function will return `text` after removing 
+     *                                                  all white spaces inside it.
+     * - removeLineBreaks(text):                        This function will return `text` after removing
+     *                                                  all line breaks and newline characters inside it.
+     * - getNumberOfOccurrencesForLetter(text, letter): This function returns the number of occurrences of
+     *                                                  `letter` inside `text`.
+     * - buildHistoFreqInfo(letter, frequency):         Builds an object encapsulating the information of a
+     *                                                  letter's frequency so that you can return it in this function
+     *                                                  so that CryptoHisto can build the histogram.
+     * 
+     * Important
+     * This function must return an array of objects created using function `buildHistoFreqInfo`.
      */
 
     return data_lang_nil;
